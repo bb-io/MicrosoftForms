df5a1b9d8b70adfee298c959d556195091d187e8
Bohdan Makarenko <bohdan.makarenko@blackbird.io>
2024-11-27 17:41:00 +0200

    Test refresh token fix

--- Apps.MicrosoftForms/Actions/FormActions.cs
+++ Apps.MicrosoftForms/Actions/FormActions.cs
@@ -95,7 +95,6 @@ public class FormActions(InvocationContext invocationContext, IFileManagementCli
         return new() { ResponsesFile = responsesFile };
     }
 
-
     //[Action("Debug", Description = "Debug")]
     //public async Task<string> Debug()
     //{
